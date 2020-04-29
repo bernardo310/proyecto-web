27171a7f7d35997ee63ee8b4988f66b52161a3bf
bernardo310 <your@email.com>
2020-04-29 17:19:16 -0500

    0

--- Express/controllers/notificaciones.js
+++ Express/controllers/notificaciones.js
@@ -1,5 +1,18 @@
+const db = require('tnc_mysql_connector');
+
 exports.getDarDeAlta = async (req, res, next) => {
-    //hacer cosas
+    const correo = req.body.correo; 
+    const reporte = req.body.reporte; 
+    const columna = req.body.columna; 
+    const valor = req.body.valor; 
+
+    // 1 insertar reporte
+    try {
+        await db.procedures.insertReporte(reporte)[0];
+    } catch(err) {
+        console.log(err);
+    }
+    
 
     res.status(200).json({
         prueba: 'yes'
