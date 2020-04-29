e943c35f715ea97a0362bc7fbcfb0a7011afca8f
Juan Manuel Alvarez <43937673+jm-alvarezs@users.noreply.github.com>
2020-04-29 16:17:01 -0500

    added cors

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const app = express();
+const cors = require("cors");
+
+const allowedOrigins = ["http://localhost:3000"];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)      
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin: "+origin;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+  })
+);
+
+
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
 
 const buscarRoutes = require('./routes/buscar');
 const notificacionesRoutes = require('./routes/notificaciones');
 
-const app = express();
-app.use(bodyParser.urlencoded({ extended:true }));
-app.use(bodyParser.json());
-
 app.use(buscarRoutes);
 app.use(notificacionesRoutes);
 
-
-app.listen(3000, () => {
-    console.log('server running port 3000');
+app.listen(4000, () => {
+    console.log('server running port 4000');
 })
