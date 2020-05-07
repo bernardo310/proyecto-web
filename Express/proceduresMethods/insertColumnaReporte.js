a823e681eadfdfeb6783f07ce2dae1c43bf9228d
orlandoxtg <orlando.torres@udem.edu>
2020-05-06 19:18:14 -0500

    Created all procedures

--- Express/proceduresMethods/insertColumnaReporte.js
+++ Express/proceduresMethods/insertColumnaReporte.js
@@ -4,16 +4,15 @@ let db;
 * @summary Call to procedure insertColumnaReporte
 * @param {Number} idReporte
 * @param {Number} idColumna
-* @param {Number} orden
 * @return {Array} Returns array of results if procedure has a SELECT 
 * @example
 *
-*	insertColumnaReporte(1, 1, 1)
+*	insertColumnaReporte(1, 1)
 */
 
-const insertColumnaReporte = async (idReporte, idColumna, orden) => {
+const insertColumnaReporte = async (idReporte, idColumna) => {
    if(!db) db = require("tnc_mysql_connector");
-   return (await db.queryProcedure('insertColumnaReporte',idReporte, idColumna, orden));
+   return (await db.queryProcedure('insertColumnaReporte',idReporte, idColumna));
 }
 module.exports = {
    insertColumnaReporte
