8bb9e440ccc9c0d3586952b8aa9e72d6cc32fe51
Bernardo <bernardocardenas98@gmail.com>
2020-05-24 17:29:49 -0500

    test notificaciones

--- Express/controllers/notificaciones.js
+++ Express/controllers/notificaciones.js
@@ -1,3 +1,5 @@
+const schedule = require('node-schedule');
+const nodemailer = require('nodemailer');
 const db = require('tnc_mysql_connector');
 
 exports.getDarDeAlta = async (req, res, next) => {
@@ -5,10 +7,11 @@ exports.getDarDeAlta = async (req, res, next) => {
     const reporte = req.body.reporte; 
     const columna = req.body.columna; 
     const valor = req.body.valor; 
+    const registros = req.body.registros
 
     // 1 insertar reporte
     try {
-        await db.procedures.insertReporte(reporte)[0];
+        await db.procedures.insertAlerta(reporte, columna, valor, correo, registros)[0];
     } catch(err) {
         console.log(err);
     }
@@ -18,3 +21,39 @@ exports.getDarDeAlta = async (req, res, next) => {
         prueba: 'yes'
     });
 }
+
+//0 0 0*/6 * * *   cada 6h
+schedule.scheduleJob('0 0*/1 * * * *', function() {
+    console.log('test');
+    //obtener gente registrada para alertas
+
+    //iterar cada uno y checar si hay registros nuevos
+
+    //si hay mas, mandar correo y actualizar n registros
+
+
+    var transporter = nodemailer.createTransport({
+        host: 'thenorthcode.com',
+        port: 465,
+        auth: {
+          user: 'orlando.torres@thenorthcode.com',
+          pass: 'Or01081999'
+        }
+      });
+      
+      var mailOptions = {
+        from: 'orlando.torres@thenorthcode.com',
+        to: 'bernardo.cardenas@udem.edu',
+        subject: 'Sending Email using Node.js',
+        text: 'That was very easy!'
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+
+})
